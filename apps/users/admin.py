2e2985aa46df9e76d3d19947d4abb680c6310698
DeeWang <wangdi.dlut@outlook.com>
2017-04-16 21:00:23 +0800

    成功添加list_display, search_field, list_filter三个插件

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('username', 'email','nickname', 'birthday', 'gender', 'address', 'phone_num', 'image')
+    search_fields = ('username', 'email','nickname', 'birthday', 'gender', 'address', 'phone_num')
+    list_filter = ('username', 'email','nickname', 'address', 'phone_num')
 
 admin.site.register(UserProfile, UserProfileAdmin)
 # Register your models here.
