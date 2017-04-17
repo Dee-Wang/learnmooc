52870aadd15add80947499d8729ab62069af8696
DeeWang <wangdi.dlut@outlook.com>
2017-04-17 18:13:47 +0800

    Add operation and course admin plug-in

--- apps/course/admin.py
+++ apps/course/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, Lesson,  Video, VideoResource
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name","description","detail" ,"degree", "learning_time", "learning_num", "favor_num", "click_num", "image", "add_time")
+    search_fields = ("name", "learning_time", )
+    list_filter = ("degree", )
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("course", "name","add_time")
+    search_fields = ("name", "course", )
+    list_filter = ("course", )
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ("course", "lesson","name","add_time")
+    search_fields = ("name", )
+    list_filter = ("course", "lesson", )
+
+
+class VideoResourceAdmin(admin.ModelAdmin):
+    list_display = ("course", "name","download","add_time")
+    search_fields = ("name",)
+    list_filter = ("course", )
+
+
+admin.site.register(Course, CourseAdmin )
+admin.site.register(Lesson, LessonAdmin )
+admin.site.register(Video, VideoAdmin )
+admin.site.register(VideoResource, VideoResourceAdmin )
--- apps/operation/admin.py
+++ apps/operation/admin.py
@@ -3,23 +3,33 @@ from django.contrib import admin
 from .models import UserWant, CourseComments, UserCourse, UserFavor, UserMessage
 
 class UserWantAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'phone_num', 'course_name', 'add_time')
+    search_fields = ('name', 'phone_num', 'course_name')
+    list_filter = ('course_name', )
 
 
 class CourseCommentsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'course', 'comments', 'add_time')
+    search_fields = ('user', 'course', )
+    list_filter = ('user', 'course', )
 
 
 class UserCourseAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'course', 'add_time')
+    search_fields = ('user', 'course', )
+    list_filter = ('user', 'course', )
 
 
 class UserFavorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'fav_id', 'fav_type', 'add_time')
+    search_fields = ('user', )
+    list_filter = ('fav_id', 'fav_type', )
 
 
 class UserMessageAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'is_read', 'message', 'add_time')
+    search_fields = ('user', 'message', )
+    list_filter = ('is_read', )
 
 
 admin.site.register(UserWant, UserWantAdmin )
