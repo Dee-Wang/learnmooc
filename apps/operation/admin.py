c333ca26f2fc5ab6b10230dc22d54e32c60f8c94
DeeWang <wangdi.dlut@outlook.com>
2017-04-17 17:26:02 +0800

    Register operation admin file

--- apps/operation/admin.py
+++ apps/operation/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
+from .models import UserWant, CourseComments, UserCourse, UserFavor, UserMessage
+
+class UserWantAdmin(admin.ModelAdmin):
+    pass
+
+
+class CourseCommentsAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserCourseAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserFavorAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserMessageAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserWant, UserWantAdmin )
+admin.site.register(CourseComments, CourseCommentsAdmin )
+admin.site.register(UserCourse, UserCourseAdmin )
+admin.site.register(UserFavor, UserFavorAdmin )
+admin.site.register(UserMessage, UserMessageAdmin )
+
 # Register your models here.
